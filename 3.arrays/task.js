82f4c7b9cc2ea54b784fdb8f0661ce25736df7e4
Nataly Vasilyeva <nvasilyeva.nv@gmail.com>
2023-02-08 09:44:23 +0500

    Первая правка

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,14 +1,13 @@
 function compareArrays(arr1, arr2) {
-   return arr1.length === arr2.length && arr1.every(i => arr1[i] === arr2[i]) 
+   return arr1.length === arr2.length && arr1.every((item, index) => item === arr2[index])
 }
 
 
 function getUsersNamesInAgeRange(users, gender) {
-  if (users.length === 0 || (gender != "мужской" && gender != "женский")) {
-    return 0
-  }
-  
-  let result = users.filter(user => user.gender === gender).map(user => user = user.age); 
-  let sum = result.reduce((a,b) => a+b);
-  return sum / result.length  
+   let result = users.filter(user => user.gender === gender).map(user =>  user.age).reduce((previousIndex, currentIndex, index, array) => { 
+     if (index === array.length - 1) {
+      return (previousIndex + currentIndex) / array.length; 
+     } else return previousIndex + currentIndex 
+   }, 0);
+  return result
 }
